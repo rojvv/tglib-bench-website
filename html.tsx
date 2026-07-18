d9d2871f0cbffe796c5267d0aff080cdc20fd2e1
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2026-07-18 14:12:11 +0530

    Comment out site verification and published time meta tags
    Commented out site verification and published time properties for clarity.

--- html.tsx
+++ html.tsx
@@ -13,9 +13,9 @@ export default (
       <link rel="stylesheet" href={`/main.css${id}`} />
       {/* <script src="/lib/lottie-player.js" /> */}
       <script src={`/main.js${id}`} />
-      <!--site_verification property must be present, but could be empty (no IVBot-side verification for now?)-->
+      {/*<!--site_verification property must be present, but could be empty (no IVBot-side verification for now?)-->*/}
       <meta property="tg:site_verification" content="g7j8/rPFXfhyrq5q0QQV7EsYWv4=">
-      <!--published_time property must be present, but you could leave content empty if no $published_date is wanted-->
+      {/*<!--published_time property must be present, but you could leave content empty if no $published_date is wanted-->*/}
       <meta property="article:published_time" content="">
       <meta property="article:author" content="https://t.me/libspeedbot">
       <meta property="telegram:channel" content="@tglibs">
