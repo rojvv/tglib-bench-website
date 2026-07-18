9fee285c05ef3d4688b99f9ad0b9c8649922eec7
Roj <rojvv@icloud.com>
2026-07-18 08:45:42 +0000

    Update html.tsx

--- html.tsx
+++ html.tsx
@@ -14,82 +14,85 @@ export default (
       {/* <script src="/lib/lottie-player.js" /> */}
       <script src={`/main.js${id}`} />
       {/*<!--site_verification property must be present, but could be empty (no IVBot-side verification for now?)-->*/}
-      <meta property="tg:site_verification" content="g7j8/rPFXfhyrq5q0QQV7EsYWv4=">
+      <meta
+        property="tg:site_verification"
+        content="g7j8/rPFXfhyrq5q0QQV7EsYWv4="
+      />
       {/*<!--published_time property must be present, but you could leave content empty if no $published_date is wanted-->*/}
-      <meta property="article:published_time" content="">
-      <meta property="article:author" content="https://t.me/libspeedbot">
-      <meta property="telegram:channel" content="@tglibs">
+      <meta property="article:published_time" content="" />
+      <meta property="article:author" content="https://t.me/libspeedbot" />
+      <meta property="telegram:channel" content="@tglibs" />
     </head>
     <body>
       <div class="article">
         <article class="article__content">
-      <div>
-        <div>
-          Download and Upload Speed Comparison of Different Telegram Client
-          Libraries
-        </div>
-        <div></div>
+          <div>
+            <div>
+              Download and Upload Speed Comparison of Different Telegram Client
+              Libraries
+            </div>
+            <div></div>
+          </div>
+          <main>
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col" class="number">
+                    <span class="null">0</span>#
+                  </th>
+                  <th scope="col">Library</th>
+                  <th scope="col">Download</th>
+                  <th scope="col">Upload</th>
+                  <th scope="col">Date</th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {results.map((v, i) => (
+                  <tr>
+                    <Idx idx={i + 1} />
+                    <td>
+                      <a href={v.library.link}>{v.library.name}</a>
+                    </td>
+                    <td class="number">
+                      <span class="null">{v.downloadMbs < 10 && "0"}</span>
+                      {v.downloadMbs.toFixed(1)} MB/s
+                    </td>
+                    <td class="number">
+                      <span class="null">{v.uploadMbs < 10 && "0"}</span>
+                      {v.uploadMbs.toFixed(1)} MB/s
+                    </td>
+                    <td class="number">
+                      {new Intl.DateTimeFormat().format(v.date)}
+                    </td>
+                    <td>
+                      <a
+                        href={`https://github.com/rojvv/tglib-bench/tree/main/${v.library.slug}`}
+                      >
+                        View Source
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </main>
+          <footer>
+            <div>
+              <span>
+                Each library was given a 2 GB file to download, and then
+                re-upload. All benchmarks were run inside GitHub-hosted runners
+                located in the United States, and connected to Telegram’s DC1
+                (United States). Machine specifications: Linux x86-64 (Ubuntu
+                24.04), 4-core CPU, 16 GB memory, 14 GB SSD storage.{" "}
+              </span>
+              <a href="https://github.com/rojvv/tglib-bench/actions/workflows/bench.yml">
+                View runs.
+              </a>
+            </div>
+          </footer>
+        </article>
       </div>
-      <main>
-        <table>
-          <thead>
-            <tr>
-              <th scope="col" class="number">
-                <span class="null">0</span>#
-              </th>
-              <th scope="col">Library</th>
-              <th scope="col">Download</th>
-              <th scope="col">Upload</th>
-              <th scope="col">Date</th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {results.map((v, i) => (
-              <tr>
-                <Idx idx={i + 1} />
-                <td>
-                  <a href={v.library.link}>{v.library.name}</a>
-                </td>
-                <td class="number">
-                  <span class="null">{v.downloadMbs < 10 && "0"}</span>
-                  {v.downloadMbs.toFixed(1)} MB/s
-                </td>
-                <td class="number">
-                  <span class="null">{v.uploadMbs < 10 && "0"}</span>
-                  {v.uploadMbs.toFixed(1)} MB/s
-                </td>
-                <td class="number">
-                  {new Intl.DateTimeFormat().format(v.date)}
-                </td>
-                <td>
-                  <a
-                    href={`https://github.com/rojvv/tglib-bench/tree/main/${v.library.slug}`}
-                  >
-                    View Source
-                  </a>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </main>
-      <footer>
-        <div>
-          <span>
-            Each library was given a 2 GB file to download, and then re-upload.
-            All benchmarks were run inside GitHub-hosted runners located in the
-            United States, and connected to Telegram’s DC1 (United States).
-            Machine specifications: Linux x86-64 (Ubuntu 24.04), 4-core CPU, 16
-            GB memory, 14 GB SSD storage.{" "}
-          </span>
-          <a href="https://github.com/rojvv/tglib-bench/actions/workflows/bench.yml">
-            View runs.
-          </a>
-        </div>
-      </footer>
-         </article>
-       </div>
     </body>
   </html>
 );
